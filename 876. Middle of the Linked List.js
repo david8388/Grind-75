b76e0cf7da3623c0492e0ac0eb1e1573143dc051
david.wu <hsienwu830808@gmail.com>
2025-02-07 11:02:42 +0800

    feat: implement middle of the linked list for 2nd solution

--- 876. Middle of the Linked List.js
+++ 876. Middle of the Linked List.js
@@ -10,6 +10,7 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
+    // 1st approach. Use two pointers.
     let slow = head
     let fast = head
     while (fast && fast.next !== null) {
@@ -17,4 +18,22 @@ var middleNode = function(head) {
         fast = fast.next.next
     }
     return slow
-};
+
+    // 2nd approach. Calculate the linked list length, determine the middle index, and return the corresponding(middle) node.
+    /*let totalLen = 0
+    let curr = head
+    while (curr) {
+        curr = curr.next
+        totalLen++
+    }
+
+    middle = head
+    let counter = 0
+    let endIndex = Math.floor(totalLen / 2)
+    while (counter < endIndex) {
+        middle = middle.next
+        counter++
+    }
+
+    return middle*/
+}
