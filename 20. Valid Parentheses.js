a9def351112802aa565cbd2b028e9e9cd4cc993a
david.wu <hsienwu830808@gmail.com>
2025-02-08 15:52:08 +0800

    feat: implement valid parentheses for 2nd solution

--- 20. Valid Parentheses.js
+++ 20. Valid Parentheses.js
@@ -1,9 +1,10 @@
-// Solution 1
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function(s) {
+    // Solution 1
+    /* 
     const brackets = s.split('')
     const closingBrackets = ['}', ')', ']']
     const openingBrackets = ['{', '(', '[']
@@ -17,13 +18,32 @@ var isValid = function(s) {
         const char = brackets[i]
         if (openingBrackets.includes(char)) {
             buckets.push(char)
-        } else if (closingBrackets.includes(char)) {
+        } else if (closingBrackets.includes(char)){
             const popEl = buckets.pop()
             const mappingEl = corresponding[char]
             if (popEl !== mappingEl) {
                 return false
             }
-        }
+        } 
     }
     return buckets.length === 0
+    */
+    // Solution 2 
+    let stack = []
+    const mapping = {
+        '{': '}',
+        '(': ')',
+        '[': ']'
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i]
+        if (mapping[char]) { // open
+            stack.push(mapping[char])
+        } else if (stack.pop() !== char) {
+            return false
+        }
+    }
+
+    return stack.length === 0
 };
